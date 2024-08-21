412d0bf7f959571d2721a139776ab8b70228d925
drakata <lsmunyuki@gmail.com>
2024-08-21 02:16:39 +0100

    Update signup.ts

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,3 +1,5 @@
+import { prisma } from '../../../lib/prisma';
+
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse): void {
